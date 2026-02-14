8a001f4d2e7c5b3f8ca07bf3e46f6f1833d44e69
GersonAparecidoLima <gersonaparecido@gmail.com>
2026-02-14 16:09:08 -0300

    ArrayList Comparable

---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch.modelos;
+
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
+    private String diretor;
+
+
+    
+ // O construtor deve receber os dados e repassar para o Titulo via super()
+    public Filme(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento); 
+    }
+
+	public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    @Override
+    public int getClassificacao() {
+        return (int) pegaMedia() / 2;
+    }
+}
---
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,55 @@
+package br.com.alura.screenmatch.principal;
+
+import java.text.CollationElementIterator;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+public class PrincipalComListas {
+	public static void main(String[] args) {
+		Filme meuFilme = new Filme("O poderoso chefão", 1970);
+		meuFilme.avalia(9);
+		Filme outroFilme = new Filme("Avatar", 2023);
+		outroFilme.avalia(6);
+		var filmeDoPaulo = new Filme("Dogville", 2003);
+		filmeDoPaulo.avalia(10);
+		Serie lost = new Serie("Lost", 2000);
+
+		ArrayList<Titulo> lista = new ArrayList<>();
+		lista.add(filmeDoPaulo);
+		lista.add(meuFilme);
+		lista.add(outroFilme);
+		lista.add(lost);
+
+		for (Titulo item : lista) {
+			System.out.println(item.getNome()); // Ou apenas System.out.println(item);
+
+			if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+				System.out.println("Classificação " + filme.getClassificacao());
+			}
+
+		}
+		
+		ArrayList<String> buscarPorArtista = new ArrayList();
+		buscarPorArtista.add("Adam Sandler");
+		buscarPorArtista.add("Paulo");
+		buscarPorArtista.add("Jaqueline");
+		System.out.println(buscarPorArtista);
+		Collections.sort(buscarPorArtista);
+		System.out.println("Ordenado");
+		System.out.println(buscarPorArtista);
+		
+		System.out.println("Lista de Titulos normal");
+		System.out.println(lista);
+		
+		System.out.println("Lista de Titulos Ordenados");
+		//Ordenar por Titulo
+		Collections.sort(lista);
+		System.out.println(lista);
+
+	}
+}
